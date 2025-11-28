2a03a2abcd3b865a554c0270ce2989810546a0b0
Johan <johanna-pham@hotmail.com>
2025-11-28 11:43:40 +0200

    start of the game: start game button, info button, appearance of the html, hanging monkey animation, images put in a folder.

---
+++ startgame/startgame.js
@@ -0,0 +1,18 @@
+/* INFO KUPLA */
+const instructionsButton = document.getElementById("instructionsButton");
+const instructionsBubble = document.getElementById("instructionsBubble");
+
+instructionsButton.addEventListener("click", () => {
+    if (instructionsBubble.style.display === "block") {
+        instructionsBubble.style.display = "none";
+    } else {
+        instructionsBubble.style.display = "block";
+    }
+});
+
+// Close bubble if clicking outside
+document.addEventListener("click", (e) => {
+    if (!instructionsButton.contains(e.target) && !instructionsBubble.contains(e.target)) {
+        instructionsBubble.style.display = "none";
+    }
+});
