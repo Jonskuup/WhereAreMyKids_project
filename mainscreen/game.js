54f3d98d11e6dc5c66fa8a41c23fe2251efd43b8
Johan <johanna-pham@hotmail.com>
2025-12-06 22:10:53 +0200

    Lisätty button LEAVE

--- mainscreen/game.js
+++ mainscreen/game.js
@@ -1,1 +1,6 @@
 
+
+// Leave button will go back to start game screen
+document.getElementById("restartButton").addEventListener("click", () => {
+    window.location.href = "../startgame/startgame.html";
+});
