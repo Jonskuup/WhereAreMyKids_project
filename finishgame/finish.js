2fe12dac7e85dbf6c11b1de0be34496c399557c1
Johan <johanna-pham@hotmail.com>
2025-12-07 18:52:29 +0200

    lisätty trophy img ja poistettu turhat pois

--- finishgame/finish.js
+++ finishgame/finish.js
@@ -1,4 +1,3 @@
-
 // Start again button will go back to start game screen
 document.getElementById("restartButton").addEventListener("click", () => {
     window.location.href = "../startgame/startgame.html";
